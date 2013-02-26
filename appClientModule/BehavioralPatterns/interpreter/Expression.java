ee14916d7e2d87f822729c7e347bfe16e9467f26
wcjung <arisu1000@gmail.com>
2013-02-26 22:17:04 +0900

    Interpreter 패턴 예제 추가

---
+++ appClientModule/BehavioralPatterns/interpreter/Expression.java
@@ -0,0 +1,8 @@
+package BehavioralPatterns.interpreter;
+
+import java.util.Map;
+
+public interface Expression {
+	public int interpret (Map<String, Expression> variables);
+
+}
---
+++ appClientModule/BehavioralPatterns/interpreter/Plus.java
@@ -0,0 +1,18 @@
+package BehavioralPatterns.interpreter;
+
+import java.util.Map;
+
+public class Plus implements Expression {
+
+	Expression leftOperand;
+    Expression rightOperand;
+    public Plus(Expression left, Expression right) { 
+        leftOperand = left; 
+        rightOperand = right;
+    }
+ 
+    public int interpret(Map<String,Expression> variables)  { 
+        return leftOperand.interpret(variables) + rightOperand.interpret(variables);
+    }
+
+}
