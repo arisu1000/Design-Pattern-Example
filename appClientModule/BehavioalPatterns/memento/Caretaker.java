af07a08a2bdef9771cee95d8b0eac90a7d1337c1
wcjung <arisu1000@gmail.com>
2013-02-26 22:40:46 +0900

    Memento 패턴 예제 추가

---
+++ appClientModule/BehavioalPatterns/memento/Caretaker.java
@@ -0,0 +1,20 @@
+package BehavioalPatterns.memento;
+
+import java.util.List;
+import java.util.ArrayList;
+class Caretaker {
+    public static void main(String[] args) {
+        List<Originator.Memento> savedStates = new ArrayList<Originator.Memento>();
+ 
+        Originator originator = new Originator();
+        originator.set("State1");
+        originator.set("State2");
+        savedStates.add(originator.saveToMemento());
+        originator.set("State3");
+        // We can request multiple mementos, and choose which one to roll back to.
+        savedStates.add(originator.saveToMemento());
+        originator.set("State4");
+ 
+        originator.restoreFromMemento(savedStates.get(1));   
+    }
+}
